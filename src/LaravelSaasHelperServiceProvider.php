b7a74887571fe012729c6b64f277728ea6790a06
faed <327399770@qq.com>
2025-09-22 09:57:57 +0800

    first commit

---
+++ src/LaravelSaasHelperServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+namespace Faed\LaravelSaasHelper;
+
+use Illuminate\Support\ServiceProvider;
+class LaravelSaasHelperServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //配置文件
+        $this->publishes([
+            $this->configPath() => config_path('laravel-saas-helper.php'),
+        ]);
+
+
+
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom($this->configPath(), 'laravel-saas-helper');
+    }
+    /**
+     * Set the config path
+     *
+     * @return string
+     */
+    protected function configPath(): string
+    {
+        return __DIR__ . '/config/laravel-saas-helper.php';
+    }
+}
---
+++ src/log/AppendRequestIdProcessor.php
@@ -0,0 +1,14 @@
+<?php
+namespace Faed\LaravelSaasHelper\log;
+class AppendRequestIdProcessor
+{
+    public function __invoke($logger): void
+    {
+        $logger->pushProcessor(function ($record) {
+            // 从请求属性中获取 request_id
+            $requestId = request()->attributes->get('request_id', 'unknown');
+            $record['message'] = '['.$requestId.']'.$record['message'];
+            return $record;
+        });
+    }
+}
